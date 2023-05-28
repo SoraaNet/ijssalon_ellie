3c996075026044fabd4e12b88a6e671bc0c5afc4
Ozan Kilicm <SoraaNet@gmail.com>
2023-05-28 11:24:23 +0200

    Try Except toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,14 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag / personen
+    except:
+        bedrag_pp = "??"
+    return f"Het bedrag per persoon is {bedrag_pp} Euro"
+
+b = int(input("Welk bedrag zit er in de fooienpot?"))
+p = int(input("Over hoeveel personen moet de pot verdeeld worden?") )
+print (fooi_pp(b,p))
